7b6c0b4408e95be4c5e5fbc7e7c64ebdbffb0b31
zhaodb <dongbao.zhao@dhc.com.cn>
2020-11-02 11:40:40 +0800

    初始化

---
+++ src/main/java/com/desgin/demo/desigindemo/DesiginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.desgin.demo.desigindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesiginDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesiginDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/desgin/demo/desigindemo/DesiginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desgin.demo.desigindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesiginDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
